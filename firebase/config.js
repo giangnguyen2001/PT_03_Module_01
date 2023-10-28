7240768358563c0540ea97e0127d3738cbfffd34
giangnguyen2001 <nguyentruonggiang131201@gmail.com>
2023-10-28 11:00:22 +0700

    AI generator (updated)

--- firebase/config.js
+++ firebase/config.js
@@ -1,25 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDRAMPJZfLC6gXrFV2J3XXKRYHoDL4LNSg",
-  authDomain: "aiimagegeneration-c2af8.firebaseapp.com",
-  databaseURL:
-    "https://aiimagegeneration-c2af8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "aiimagegeneration-c2af8",
-  storageBucket: "aiimagegeneration-c2af8.appspot.com",
-  messagingSenderId: "559175585623",
-  appId: "1:559175585623:web:9c2fc49740fca34e237812",
-  measurementId: "G-0QKDQ2REMM",
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDRAMPJZfLC6gXrFV2J3XXKRYHoDL4LNSg",
+//   authDomain: "aiimagegeneration-c2af8.firebaseapp.com",
+//   databaseURL:
+//     "https://aiimagegeneration-c2af8-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "aiimagegeneration-c2af8",
+//   storageBucket: "aiimagegeneration-c2af8.appspot.com",
+//   messagingSenderId: "559175585623",
+//   appId: "1:559175585623:web:9c2fc49740fca34e237812",
+//   measurementId: "G-0QKDQ2REMM",
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
-export default analytics;
+// export default analytics;
--- src/app.js
+++ src/app.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from "react";
-import firebase from "firebase/app";
+// import React, { useEffect, useState } from "react";
+// import firebase from "firebase/app";
 
-const App = () => {
-  const [user, setUser] = useState(null);
+// const App = () => {
+//   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    // Đăng ký một hàm nghe sự kiện trạng thái đăng nhập thay đổi
-    const unsubscribe = firebase.auth().onAuthStateChanged((authUser) => {
-      if (authUser) {
-        // Người dùng đã đăng nhập
-        setUser(authUser);
-      } else {
-        // Người dùng đã đăng xuất
-        setUser(null);
-      }
-    });
+//   useEffect(() => {
+//     // Đăng ký một hàm nghe sự kiện trạng thái đăng nhập thay đổi
+//     const unsubscribe = firebase.auth().onAuthStateChanged((authUser) => {
+//       if (authUser) {
+//         // Người dùng đã đăng nhập
+//         setUser(authUser);
+//       } else {
+//         // Người dùng đã đăng xuất
+//         setUser(null);
+//       }
+//     });
 
-    return () => {
-      // Hủy đăng ký sự kiện khi unmount component
-      unsubscribe();
-    };
-  }, []);
+//     return () => {
+//       // Hủy đăng ký sự kiện khi unmount component
+//       unsubscribe();
+//     };
+//   }, []);
 
-  const handleLogout = async () => {
-    try {
-      await firebase.auth().signOut();
-      // Đăng xuất thành công
-    } catch (error) {
-      // Xử lý lỗi nếu có
-      console.error(error);
-    }
-  };
+//   const handleLogout = async () => {
+//     try {
+//       await firebase.auth().signOut();
+//       // Đăng xuất thành công
+//     } catch (error) {
+//       // Xử lý lỗi nếu có
+//       console.error(error);
+//     }
+//   };
 
-  return (
-    <div>
-      {user ? (
-        <div>
-          <p>Xin chào, {user.email}</p>
-          <button onClick={handleLogout}>Đăng xuất</button>
-        </div>
-      ) : (
-        <Login />
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {user ? (
+//         <div>
+//           <p>Xin chào, {user.email}</p>
+//           <button onClick={handleLogout}>Đăng xuất</button>
+//         </div>
+//       ) : (
+//         <Login />
+//       )}
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
